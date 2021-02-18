de1b9bafb3715e09a6126c42e2d2965b38be960a
Jay Serrano <jayserrano@jays-mac-mini.attlocal.net>
2021-02-18 13:10:08 -0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+  
+// function outter(){
+//     let movie = 'automan';
+
+//     function inner(){
+//         let movie = "pirates of the caribean"
+       
+//          function extrain(){
+//             //  let movie = "javier the great";
+//              console.log(movie.toUpperCase());
+//          }
+//          extrain()
+//     }
+//     inner();
+// }
+
+function TodoList(){
+    let todos = [];
+    let username = '';
+
+    function addTodo(){   
+
+    }
+    function removeTodo(){
+
+    }
+    function updateTodo(){
+
+    }
+}
+
+// let name = 'javier';
+// console.log(name);
+
+  
